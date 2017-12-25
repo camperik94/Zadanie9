8d55c4b0c557355bde5a9232884d29295d1bc012
camperik94 <tomaszjach94@gmail.com>
2017-12-25 15:17:29 +0100

    Final

--- src/Potegi.java
+++ src/Potegi.java
@@ -1,4 +1,50 @@
 
 public class Potegi {
+	private int liczba = 2;
+	private int potega;
+	private int liczbaor;
+	
+
+	public int getLiczbaor() {
+		return liczbaor;
+	}
+
+	public void setLiczbaor(int liczbaor) {
+		this.liczbaor = liczbaor;
+	}
+
+	public int getLiczba() {
+		return liczba;
+	}
+
+	public void setLiczba(int liczba) {
+		this.liczba = liczba;
+	}
+
+	public int getPotega() {
+		return potega;
+	}
+
+	public void setPotega(int potega) {
+		this.potega = potega;
+	}
+
+	public Potegi(int liczbaor) {
+		this.liczbaor = liczbaor;
+	}
+	
+	public void funkcja() {
+		int zmienna = 2;
+		for(int i = zmienna; i < liczbaor; i++) {
+			
+			System.out.println(zmienna);
+			zmienna = zmienna * liczba;
+			
+			if(zmienna > liczbaor) {
+				break;
+			}
+		}
+	}
+	
 
 }
--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
 public class Zadanie {
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Podaj liczbe ograniczajaca");
+		
+		int liczb = in.nextInt();
+		
+		Potegi pot = new Potegi(liczb);
+		
+		pot.funkcja();
 	}
 }
